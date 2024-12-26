6a67b0fe21d6db3f612fdc9a39d9875569c63141
Oladipupo <dipoakanji57@gmail.com>
2024-12-26 18:05:16 +0100

    Added typescript

--- Chat-Application/src/main.ts
+++ Chat-Application/src/main.ts
@@ -1,1 +1,13 @@
 import './style.css'
+import { input } from './modules/module.ts'
+import { dialogue } from './modules/module.ts'
+import { submit } from './modules/module.ts'
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault()
+  const toAppend = document.createElement("div")
+  toAppend.className = "dialog"
+  toAppend.innerHTML = `${input.value}`
+  dialogue.appendChild(toAppend)
+  input.value = ""
+})
---
+++ Chat-Application/src/modules/module.ts
@@ -0,0 +1,3 @@
+export const input: HTMLInputElement = document.getElementById("chat-input") as HTMLInputElement
+export const dialogue: HTMLDivElement = document.getElementById("text") as HTMLDivElement
+export const submit: HTMLButtonElement = document.getElementById("btn") as HTMLButtonElement
